514080fa4a89829a407810be4c2c1c16e7245ba1
Tvisher <tvisher@mail.ru>
2022-04-05 15:34:57 +0600

    start clinic

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+import Swiper, {
+    Navigation,
+    Manipulation
+} from 'swiper';
+import AirDatepicker from 'air-datepicker';
+import localeEn from 'air-datepicker/locale/en.js';
