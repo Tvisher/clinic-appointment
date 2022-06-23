d4442971fbc29353bb019f2ce3f2c1edbb9bc541
Tvisher <tvisher@mail.ru>
2022-06-23 13:29:14 +0600

    js fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 // import AirDatepicker from 'air-datepicker';
-import localeEn from 'air-datepicker/locale/en.js';
+// import localeEn from 'air-datepicker/locale/en.js';
 //глобальное обьявление тултипов
 import tippy from 'tippy.js';
 window.tippy = tippy;
@@ -124,13 +124,6 @@ document.addEventListener('click', (e) => {
         document.querySelector('.modal__wrapper.show').classList.remove('show');
     }
 
-    //Скрываем текстовое поле с диапозоном дат при выборе нужного диапозона
-    if (target.closest('[data-selected-range]')) {
-        let currentTimeRange = target.closest('[data-selected-range]').querySelector('.list-item__name').textContent;
-        let textBlock = document.querySelector('.date-range__select-text')
-        textBlock.innerText = currentTimeRange;
-        textBlock.classList.add('show');
-    }
     // Скрываем выпадающее меню при нажатии куда либо
     if (document.querySelector('.date-range__date-list.show')) {
         document.querySelector('.date-range__date-list').classList.remove('show');
@@ -189,39 +182,6 @@ function vanilaToggle(toggleContent, minMobSize, minPcSize, event) {
         });
     }
 }
-
-//Анимация инпутов с placeholder выезжающим за пределы поля инпута
-//Вызывать при динамической отрисовке инпутов
-// function getAnimationinputs() {
-//     const stylinginputs = document.querySelectorAll('[data-styles-field]');
-//     if (stylinginputs) {
-//         stylinginputs.forEach(input => {
-//             const inputpParent = input.parentNode;
-//             const transformtext = inputpParent.querySelector('.styles-text');
-//             input.addEventListener('focus', (e) => {
-//                 inputpParent.classList.add('focus');
-//                 transformtext && transformtext.classList.add('fixed');
-
-//                 input.addEventListener('blur', (e) => {
-//                     const inputValue = e.target.value.trim();
-//                     inputpParent.classList.remove('focus');
-//                     if (inputValue.length === 0) {
-//                         transformtext.classList.remove('fixed');
-//                     }
-//                 }, { once: true });
-//             });
-//             //Добавление класса к инпуту если он заполнен
-//             const inputValue = input.value.trim();
-//             if (inputValue.length === 0) {
-//                 transformtext.classList.remove('fixed');
-//             } else {
-//                 transformtext.classList.add('fixed');
-//             }
-//         });
-//     }
-// }
-// window.getAnimationinputs = getAnimationinputs;
-// window.getAnimationinputs();
 
 
 // Фикс функции открытия тоглящихся элементов при ресайзе
